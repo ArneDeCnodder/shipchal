5cdcc4e9a076bc3ee69314e4004a70b5173596c6
Arne De Cnodder <57662351+ArneDeCnodder@users.noreply.github.com>
2020-12-31 13:40:45 +0100

    Update index.sql

--- sql/index.sql
+++ sql/index.sql
@@ -1,11 +1,15 @@
-CREATE SCHEMA SHIPPING
-CREATE TABLE `Persoon` (
-   `personid` int(11) NOT NULL,
-   `first_name` varchar(20) NOT NULL,
-   `last_name` varchar(20) NOT NULL);
-INSERT INTO Persoon (`personid`, `first_name`, `last_name`) VALUES
- (1, 'Arne', 'De Cnodder'),
- (2, 'Maarten', 'Struyf'),
- (5, 'Rutger', 'Mols'),
- (4, 'Bram', 'Verbruggen'),
- (3, 'Filippo', 'Bagnoli');
+CREATE SCHEMA SHIPPING;
+CREATE TABLE Persoon (
+    personid int(11) NOT NULL PRIMARY KEY,
+    first_name varchar(20) NOT NULL,
+    last_name varchar(20) NOT NULL);
+INSERT INTO Persoon (personid, first_name, last_name) 
+VALUES (1, 'Arne', 'De Cnodder');
+INSERT INTO Persoon (personid, first_name, last_name)
+VALUES (2, 'Maarten', 'Struyf');
+INSERT INTO Persoon (personid, first_name, last_name)
+VALUES (5, 'Rutger', 'Mols');
+INSERT INTO Persoon (personid, first_name, last_name)
+VALUES (4, 'Bram', 'Verbruggen');
+INSERT INTO Persoon (personid, first_name, last_name)
+VALUES (3, 'Filippo', 'Bagnoli');
