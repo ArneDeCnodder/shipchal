b7054bb08a02ab409bb8ccd98ebd504af3be135e
Arne De Cnodder <57662351+ArneDeCnodder@users.noreply.github.com>
2020-12-31 14:22:19 +0100

    Update index.sql

--- sql/index.sql
+++ sql/index.sql
@@ -1,15 +1,16 @@
 CREATE SCHEMA SHIPPING;
-CREATE TABLE Persoon (
+use SHIPPING;
+CREATE TABLE SHIPPING.Persoon (
     personid int(11) NOT NULL PRIMARY KEY,
     first_name varchar(20) NOT NULL,
     last_name varchar(20) NOT NULL);
-INSERT INTO Persoon (personid, first_name, last_name) 
+INSERT INTO SHIPPING.Persoon (personid, first_name, last_name) 
 VALUES (1, 'Arne', 'De Cnodder');
-INSERT INTO Persoon (personid, first_name, last_name)
+INSERT INTO SHIPPING.Persoon (personid, first_name, last_name)
 VALUES (2, 'Maarten', 'Struyf');
-INSERT INTO Persoon (personid, first_name, last_name)
+INSERT INTO SHIPPING.Persoon (personid, first_name, last_name)
 VALUES (5, 'Rutger', 'Mols');
-INSERT INTO Persoon (personid, first_name, last_name)
+INSERT INTO SHIPPING.Persoon (personid, first_name, last_name)
 VALUES (4, 'Bram', 'Verbruggen');
-INSERT INTO Persoon (personid, first_name, last_name)
+INSERT INTO SHIPPING.Persoon (personid, first_name, last_name)
 VALUES (3, 'Filippo', 'Bagnoli');
